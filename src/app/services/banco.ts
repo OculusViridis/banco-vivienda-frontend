6b21053ed66d4dcdf4c56b7a7eb9830d16dcf356
Pablo Contreras <pfer.contreras@gmail.com>
2026-04-24 12:18:08 -0600

    Conexión a producción

--- src/app/services/banco.ts
+++ src/app/services/banco.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class BancoService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = 'https://banco-vivienda-api.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
